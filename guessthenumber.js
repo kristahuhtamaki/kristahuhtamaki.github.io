2ac0d793b2d238f8f6d02bfcf2e1250935f7129c
krhuhta <kristahuhtamaki@Kristas-MacBook-Air-2.local>
2017-02-28 09:05:29 +0200

    korjauksia

--- guessthenumber.js
+++ guessthenumber.js
@@ -1,29 +1,31 @@
 function getRandomInteger(min, max) {
-	return Math.floor(Math.random() * (max - min + 1) ) + min;
+    "use strict";
+	return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
 var correctNumber = getRandomInteger(1, 10);
-
 function compareNumbers(first, second) {
-	return (first == second);
+    "use strict";
+	return (first === second);
 }
-
-document.getElementById("button").addEventListener("click", guessTheNumber);
 
 
 function guessTheNumber() {
 
+    "use strict";
 	var usersNumber = document.getElementById("number").value;
     
-	if (!Number.isInteger(usersNumber) && (usersNumber <= 1 && usersNumber >= 10)){
-		alert("Syötetty luku ei kelpaa");
+	if (!Number.isInteger(usersNumber) && usersNumber <= 1 && usersNumber >= 10) {
+		window.alert("Syötetty luku ei kelpaa");
 	}
 
-	if(compareNumbers(correctNumber, usersNumber)){
+	if (compareNumbers(correctNumber, usersNumber)) {
 		window.alert("Arvasit oikein!");
-	}else{
+	} else {
 		window.alert("Arvasit väärin...");
 	}
 
-	numberToGuess = getRandomInteger(1, 10);
+	correctNumber = getRandomInteger(1, 10);
 }
+
+document.getElementById("button").addEventListener("click", guessTheNumber);
