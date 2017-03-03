b6925f12c3477b59a8fe66c75e6200d7b6b180d3
krhuhta <kristahuhtamaki@Kristas-MacBook-Air-2.local>
2017-03-03 20:31:00 +0200

    muutokset

--- slideshow.js
+++ slideshow.js
@@ -3,100 +3,84 @@ window.onload = function () {
     var x = null;
     $.getJSON("https://project-3800075639156052903.firebaseio.com/.json", function (data) {
     
-        x = data;
-        slideshow();
+    x = data;
+    slideshow();
     
   
     });
-function slideshow() {
-    var index = 0;
-    function seuraava(n) {
-        if (n < 2) {
-            n++;
-        } else {
-            n = 0;
-        }
-        return n;
     
-    } ;
-    function edellinen(n){
-        if(n>0){
-            n--;
-        }
-        else {
-            n=2
-        }
-        return n
-    };
+    function slideshow() {
+        var index = 0;
+        function next(n) {
+            if (n < 2) {
+                n++;
+            } else {
+                n = 0;
+            }
+            return n;
     
-  var isActive = true
-    function Mytimer() {
-        
-        
-   
- 
-    $("#news").fadeOut(300,function(){
-     $("#news").html("<div class= 'changing-text'> "+
-       "<p>" + x[index].title+"&nbsp;&nbsp;&nbsp" + x[index].date + 
-       "</p> <br>" +"<p>" + x[index].article + " </p> </div>");
-        $("#news").fadeIn();
-        
-
-    })
-   
-};
+        } ;
+        function previous(n){
+            if(n>0){
+                n--;
+            } else {
+                n=2
+            }
+            return n
+        };
+    
+    
+        var active = true
+    
+        function Mytimer() {
+            $("#news").fadeOut(350,function(){
+                $("#news").html("<div class= 'changing-text'> "+"<p>" + x[index].title+"&nbsp" + x[index].date + "</p> <br>" +"<p>" + x[index].article + " </p> </div>");
+                $("#news").fadeIn();
+            })  
+        };
   
       
+        document.getElementById("button").addEventListener("click",     function(){
+            index = next(index);
+         
+            Mytimer();
+            active = false;
+            clearInterval(interval);
+            $("#button3").html("Toista");
+        });
+     
+        document.getElementById("button2").addEventListener("click", function(){
+            index = previous(index);
+            Mytimer();
+            active = false;
+            clearInterval(interval);
+            $("#button3").html("Toista");
+        });    
+    
         
-       
-      
-    document.getElementById("button").addEventListener("click", function(){
-     index = seuraava(index);
-         
-       Mytimer();
-       isActive = false;
-        clearInterval(voivittu);
-        $("#button3").html("Jatka");
+    
+        $("#button3").click(function(){
+            if(active) {
+                active = false;
+                clearInterval(interval);
+                $("#button3").html("Toista");
+            } else {
+                active = true;
+                interval = setInterval(function(){
+                    index = next(index);
+                    Mytimer();
+                },4000);
+                $("#button3").html("Tauota");
+            };
 
-});
- 
-               
-             document.getElementById("button2").addEventListener("click", function(){
-     index = edellinen(index);
-    
-         
-        Mytimer();
-     isActive = false;
-        clearInterval(voivittu);
-        $("#button3").html("Jatka");
+        }); 
 
-});    
-    
-    
-    $("#button3").click(function(){
-        if(isActive){
-            isActive = false;
-        clearInterval(voivittu);
-        $("#button3").html("Jatka");
-        }
-        else{
-            isActive = true;
-        voivittu = setInterval(function(){
-         index = seuraava(index);
-        Mytimer();
         
-        },4000);
-             $("#button3").html("Pysäytä");
-         
-        };
-
-}); 
-    
-      var voivittu = setInterval(function(){
-         index = seuraava(index);
-        Mytimer();
+        var interval = setInterval(function(){
+            index = next(index);
+            Mytimer();
         
         },4000);
         
     }
-     }
+}
