fdbf3f220c6c350bb78a55c2f99c77a0dc1ea689
krhuhta <kristahuhtamaki@Kristas-MacBook-Air-2.local>
2017-03-03 12:40:28 +0200

    slideshow

--- slideshow.js
+++ slideshow.js
@@ -1,113 +1,107 @@
-$(document).ready(function(){
-	var loop;
-    $.getJSON('https://project-8145367811882739054.firebaseio.com/.json',function(res){
-        loadSettings();
-        $(window).unload(saveSettings);
-        addArticle(res.articles);
-    })
-});
-//array for the articles
-var htmlTexts = [];
-//number of articles that come back
-var articleAmount = 0;
-//Id for the article
-var article_index = -1;
-// Function that adds the article to the correct div and starts the loop
-// with the returned articles
-function addArticle(data) {
-	for (var i = 0; i < data.length; i++) {
-		articleAmount = data.length;
-		htmlTexts.push('<div class="col-xs-12" id="newsArticle-'+i+'"><h3 id="newsHead-'+i+'">'+data[i].text.title+'</h3><p id="newsTime-'+i+'">'+data[i].text.date+'</p><p id="newsText-'+i+'">'+data[i].text.article+'</p><h3 id="newsAuthor-'+i+'">Author: '+data[i].text.author+'</h3></div>');
-	}
-	$('#newsArticle').append(htmlTexts);
-	for (var i = 0; i < data.length; i++) {
-		$('#newsArticle-' + i).hide();
-	}
-	loopArticles();
-}
-//function that fades in a article with a specific index
-function showArticle(index) {
-	$('#newsArticle-'+index).fadeIn(2000);
-}
-// function that loops the articles. Starts witb hiding the 
-// last seen article, then defines what the next index should be
-// and then fades in the next article
-
-//count 
-var count = 0;
-
-function loopArticles() {
-    if(count === 0) {
-        article_index ++;
-        $('#newsArticle-'+article_index).fadeIn(2000);
-        count ++;
-    };
-    loop = setInterval(function(){
-		$('#newsArticle-' + (article_index)).hide();
-        if(article_index == articleAmount - 1) {
-            article_index = 0;
+window.onload = function(){
+   
+    var x = null;
+    $.getJSON("https://paukkea2.firebaseio.com/.json",function (data) {
+    
+     x = data;
+        slideshow()
+    
+  
+    });
+function slideshow(){
+    var saatana = 0;
+    function seuraava(n){
+        if(n<2) {
+            n++;
         }
         else {
-            article_index ++;
+            n=0;
         }
-        $('#newsArticle-'+article_index).fadeIn(2000);
-	},3000);
+        return n
+        
+    };
+    function edellinen(n){
+        if(n>0){
+            n--;
+        }
+        else {
+            n=2
+        }
+        return n
+    };
+    
+  var isActive = true
+    function Mytimer() {
+        
+        
+        
+        
+    
+     
+          
+ 
+    $("#news").fadeOut(300,function(){
+     $("#news").html("<div class= 'otsikko'> "+
+       "<p>" + x[saatana].otsikko+"&nbsp;&nbsp;&nbsp" + x[saatana].date + 
+       "</p> <br>" +"<p>" + x[saatana].contents + " </p> </div>");
+        $("#news").fadeIn();
+        
+
+    })
+   
 };
-//
-$('#start_stop').click('click', function(e) {
-    var $this = $(this);
-    $this.toggleClass('Play');
-    if($this.hasClass('Play')){
-        clearInterval(loop);
-        $this.text("Start");
-    }
-    else {
-        loopArticles();
-        $this.text("Stop");
-    }
-});
+  
+      
+        
+       
+      
+    document.getElementById("button").addEventListener("click", function(){
+     saatana = seuraava(saatana);
+         
+       Mytimer();
+       isActive = false;
+        clearInterval(voivittu);
+        $("#button3").html("Jatka");
 
-$('#next').click('click',function(e) {
-    var $this = $(this);
-    clearInterval(loop);
-    $('#start_stop').addClass('Play');
-    $('#start_stop').text("Start");
-    $('#newsArticle-' + (article_index)).hide();
-    if(article_index == articleAmount - 1) {
-            article_index = 0;
-     }
-    else {
-        article_index ++;
-    }
-    $('#newsArticle-'+article_index).fadeIn(2000);
-})
-
-$('#previous').click('click',function(e) {
-    var $this = $(this);
-    clearInterval(loop);
-    $('#start_stop').addClass('Play');
-    $('#start_stop').text("Start");
-    $('#newsArticle-' + (article_index)).hide();
-    if(article_index == 0) {
-            article_index = articleAmount - 1;
-     }
-    else {
-        article_index --;
-    }
-    $('#newsArticle-'+article_index).fadeIn(2000);
 });
-//function that loads the last viewed article from local storage
-function loadSettings () {
-    if(localStorage.articleNumber === "NaN") {
-        article_index = -2;
+ 
+               
+             document.getElementById("button2").addEventListener("click", function(){
+     saatana = edellinen(saatana);
+    
+         
+        Mytimer();
+     isActive = false;
+        clearInterval(voivittu);
+        $("#button3").html("Jatka");
+
+});    
+    
+    
+    $("#button3").click(function(){
+        if(isActive){
+            isActive = false;
+        clearInterval(voivittu);
+        $("#button3").html("Jatka");
+        }
+        else{
+            isActive = true;
+        voivittu = setInterval(function(){
+         saatana = seuraava(saatana);
+        Mytimer();
+        
+        },4000);
+             $("#button3").html("Pysäytä");
+         
+        };
+
+}); 
+    
+      var voivittu = setInterval(function(){
+         saatana = seuraava(saatana);
+        Mytimer();
+        
+        },4000);
+        
     }
-    else {
-        article_index = localStorage.articleNumber;
-    }
-}
-//function that saves the last seen article to local storage
-function saveSettings() {
-    if(localStorage.articleNumber !== NaN) {
-        localStorage.articleNumber = article_index - 1;
-    }
-}
+     }
