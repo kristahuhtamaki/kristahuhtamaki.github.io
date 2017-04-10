d0457ee1e44ab71bcc3d094e4d83e74eac33bc7a
krhuhta <kristahuhtamaki@Kristas-MacBook-Air-2.local>
2017-04-10 20:51:16 +0300

    VJP game

---
+++ game/js/animation.js
@@ -0,0 +1,196 @@
+$(document).ready(function() {
+  var canvas = document.createElement('canvas');
+  var ctx = canvas.getContext("2d");
+  canvas.width = windowWidth;
+  canvas.height = windowHeight;
+  document.body.appendChild(canvas);
+  addEnemy();
+  addEnemy();
+  addEnemy();
+  addEnemy();
+  addEnemy();
+  setInterval(addEnemy, 3000)
+        
+
+  
+  
+  var backgroundReady = false;
+  var backgroundImage = new Image();
+  backgroundImage.onload = function () {
+  backgroundReady = true;
+  };
+  backgroundImage.src = "imgs/keskusta.tiff";
+
+    
+//Nappulat
+  function button(text, line, x, y){
+      this.text = text;
+      this.line = line;
+      this.x = x;
+      this.y = y;
+  }
+    
+    
+    
+// Nappuloiden kuvat
+  var buReady = false;
+  var buImage = new Image();
+  buImage.onload = function () {
+  buReady = true;
+  };
+  buImage.src = "imgs/green.png";
+    
+  var newGame = new button("Uusi peli", 0, 256 + 20 , height + 18);
+
+  var newEnemy = new button("Uusi vihollinen", 0, 512 + 30, height + 18);
+  
+  var buttons = [newGame, newEnemy];
+    
+    
+        
+  //Sprite
+  function sprite (options, b) {
+    var that = {};
+    frameIndex = b.line;			
+    that.context = options.context;
+    that.width = options.width;
+    that.height = options.height;
+    that.image = options.image;
+    that.render = function (b) {
+        that.context.drawImage(
+           that.image,
+           0,
+           frameIndex * that.height / 2,
+           that.width,
+           that.height / 2,
+           b.x,
+           b.y,
+           that.width,
+           that.height / 2);
+    };
+    return that;
+}
+    
+    
+
+  
+    
+//Hiiren sijainti
+function getMousePosition(canvas, evt) {
+    var rect = canvas.getBoundingClientRect();
+    return {
+      x: evt.clientX - rect.left,
+      y: evt.clientY - rect.top
+    };
+}
+ 
+//Näppäinten painaminen
+  var keysDown = {};
+    
+function buttonPressed(x,y){
+    for(k = 0; k < buttons.length; k++){
+        var b = buttons[k];
+        if(x < b.x + 256 && x > b.x && y < b.y + 128 && y > b.y){
+            b.line = 1;
+            switch(b.text){
+                case "Uusi peli":
+                    pReset();
+                    eReset();
+                    break;
+                case "Uusi vihollinen":
+                    addEnemy();
+                    break;
+                    
+           }
+        }
+    }
+}    
+  window.addEventListener("keydown", function(e) {
+     keysDown[e.keyCode] = true;
+  }, false);
+  
+  window.addEventListener("keyup", function(e) {
+    delete keysDown[e.keyCode];
+  });
+
+  window.addEventListener("keydown",function(e){
+      if([37,38,39,40].indexOf(e.keyCode)>-1){
+          e.preventDefault();
+      };
+  });
+  canvas.addEventListener("mousedown", function(e){
+     removeEnemy(getMousePosition(canvas,e).x, getMousePosition(canvas,e).y);
+     buttonPressed(getMousePosition(canvas,e).x, getMousePosition(canvas,e).y);
+  })
+  canvas.addEventListener("mouseup", function(e){
+      for(i = 0; i < buttons.length; i ++){
+          buttons[i].line = 0;
+      }
+  })
+  
+
+    
+var render = function() {
+
+  if (backgroundReady) {
+		ctx.drawImage(backgroundImage, 0, 0);
+	}
+  ctx.fillStyle = '#66ff66';
+  
+  drawPlayer(ctx, keysDown);
+  drawEnemies(ctx);
+  ctx.fillRect(0, height, windowWidth, 100);
+  if(buReady){
+      for(k = 0; k < buttons.length; k++) {
+          var bu = buttons[k];
+          var buttonSprite = sprite({
+            context: ctx,
+            width: 256,
+            height: 128,
+            image: buImage   
+        }, bu);
+        buttonSprite.render(bu); 
+        ctx.fillStyle = '#006600';
+        ctx.font = "40px Sans Serif";
+        ctx.fillText(bu.text, bu.x + 27, bu.y + 45, 200);
+      } 
+  }
+};
+
+    
+    
+//Update
+var update = function(delta) {
+    if (38 in keysDown) {
+       movePlayer("up");
+    } 
+    if (40 in keysDown) {
+       movePlayer("down");
+    }
+    if (37 in keysDown) {
+      movePlayer("left");
+    }
+    if (39 in keysDown) {
+      movePlayer("right");
+    }
+    moveEnemies()
+};
+
+//Animation
+var main = function() {
+  var now = Date.now();
+  var delta = now - then;
+
+  update(delta / 1000);
+  render();
+
+  then = now;
+
+  // Request to do this again ASAP
+  requestAnimationFrame(main);
+};
+  
+var then = Date.now();
+main();
+  
+});
---
+++ game/js/enemy.js
@@ -0,0 +1,254 @@
+function enemy(x,y,r,speed,dir,frame,row,ticks) {
+ this.x = x;
+ this.y = y;
+ this.r = r;
+ this.speed = speed;
+ this.dir = dir;
+ this.frame = frame;
+ this.row = row;
+ this.ticks = ticks;
+}
+
+function eReset() {
+    enemySpeed = 2;
+    enemies = [];
+    addEnemy();
+    addEnemy();
+    addEnemy();
+    addEnemy();
+    addEnemy();
+}
+
+var canAdd = true;
+var eMod = -5
+var eSize = 20;
+var enemySpeed = 2;
+var enemies = []; 
+
+
+
+function direction(ang){
+    this.ang = ang;
+    this.x = Math.cos(ang);
+    this.y = Math.sin(ang);
+}
+
+
+function spriteE (options, enemy) {			
+    var that = {};  
+    frameIndex = enemy.frame,
+    rowIndex = enemy.row,
+    tickCount = enemy.ticks,
+    ticksPerFrame = 0,
+    numberOfFrames = options.numberOfFrames || 1,
+    
+					
+    that.context = options.context;
+    that.width = options.width;
+    that.height = options.height;
+    that.image = options.image;
+    that.loop = options.loop;
+    
+    that.update = function (enemy) {
+        enemy.ticks += 1;
+        
+			
+        if (tickCount > ticksPerFrame) {
+            
+        	enemy.ticks = 0;
+        	
+            if(rowIndex == 3 && frameIndex == 3){
+                enemy.row = (enemy.row + 1) % 4;
+                enemy.frame = 0; 
+            }else if (frameIndex < numberOfFrames - 1) {	
+                // Go to the next frame
+                enemy.frame += 1;
+            }	else if (that.loop) {
+                enemy.row = (enemy.row + 1) % 4;
+                enemy.frame = 0;
+                
+            }
+        }
+    };
+    
+    that.render = function (enemy) {
+
+        // Draw the animation
+        that.context.drawImage(
+           that.image,
+           frameIndex * that.width / numberOfFrames,
+           rowIndex * that.height / 4,
+           that.width / 5,
+           that.height / 4,
+           enemy.x - enemy.r,
+           enemy.y - enemy.r,
+           enemy.r * 2,
+           enemy.r * 2);
+    };
+    
+      
+
+    return that;
+ }
+
+function removeEnemy(clickX, clickY) {
+     for(k = 0; k < enemies.length; k++){
+        var distance = Math.sqrt(Math.pow((clickX - enemies[k].x),2) + Math.pow((clickY - enemies[k].y),2));
+            if(distance < eSize){
+                enemies.splice(k,1);
+                canAdd = true;
+                }  
+     }
+}
+
+
+function drawEnemies(ctx) {
+  var EReady = false;
+  var EImage = new Image();
+  EImage.onload = function () {
+  EReady = true;
+  };
+  EImage.src = "imgs/skullball.png";
+    
+  
+
+  for(i = 0; i < enemies.length; i++){
+    var e = enemies[i];
+    var E = spriteE({
+        context: ctx,
+        width: 370,
+        height: 288,
+        image: EImage,
+        loop: true,
+        numberOfFrames: 5,
+        
+    },e);
+      E.update(e)
+      E.render(e)
+  }
+
+}
+
+
+function moveEnemies() {
+     for(i = 0; i < enemies.length; i++){
+         var e = enemies[i];
+         var oldX = e.x;
+         var oldY = e.y;
+         e.x = e.x + (e.dir.x * 2);
+         e.y = e.y + (e.dir.y * 2);
+         
+         if(e.x < e.r + eMod){
+             e.x = e.r  + eMod;
+             e.dir = new direction(Math.PI - (e.dir.ang % (2 * Math.PI)) );
+         }
+         if(e.x + eMod> width - e.r){
+             e.x = width - e.r - eMod;
+             e.dir = new direction(Math.PI - (e.dir.ang % (2 * Math.PI)) );
+             
+         }
+         if(e.y < e.r + eMod){
+             e.y = e.r + eMod;
+             e.dir = new direction(-e.dir.ang);
+         }
+         if(e.y + eMod > height - e.r){
+             e.y = height - e.r - eMod;
+             e.dir = new direction(-e.dir.ang);
+         }
+         
+         for(k = 0; k < enemies.length; k++){
+            var distance = Math.sqrt(Math.pow((e.x-enemies[k].x),2) + Math.pow((e.y - enemies[k].y),2));
+             if (!(e.x == enemies[k].x && e.y == enemies[k].y)){
+                 
+                  if(distance < 2 * e.r + eMod){
+                 e.x = oldX;
+                 e.y = oldY;
+                 var angle = 0;
+                 var en = enemies[k];
+                      
+                 if(e.x >= en.x && e.y <= en.y){
+                     angle = Math.atan((e.y - en.y)/(e.x - en.x));      
+                 } else if(e.x <= en.x && e.y <= en.y){
+                     angle = Math.atan((e.y - en.y)/(e.x - en.x)) + Math.PI;
+                 } else if(e.x >= en.x && e.y >= en.y){
+                     angle = Math.atan((e.y - en.y)/(e.x - en.x));
+                 } else if(e.x <= en.x && e.y >= en.y){
+                     angle = Math.atan((e.y - en.y)/(e.x - en.x)) + Math.PI;
+                 }
+             
+             e.dir = new direction(angle);
+             en.dir = new direction(angle + Math.PI)
+                      
+                      
+             }
+           }
+         }
+         
+         if(Math.sqrt(Math.pow((e.x-p.x),2) + Math.pow((e.y-p.y),2)) < p.r + e.r + plMod + eMod){
+             e.x = oldX;
+             e.y = oldY;
+             var angle = 0;
+             
+                 if(e.x >= p.x && e.y <= p.y){
+                     angle = Math.atan((e.y - p.y)/(e.x - p.x));      
+                 } else if(e.x <= p.x && e.y <= p.y){
+                     angle = Math.atan((e.y - p.y)/(e.x - p.x)) + Math.PI;
+                 } else if(e.x >= p.x && e.y >= p.y){
+                     angle = Math.atan((e.y - p.y)/(e.x - p.x));
+                 } else if(e.x <= p.x && e.y >= p.y){
+                     angle = Math.atan((e.y - p.y)/(e.x - p.x)) + Math.PI;
+                 }
+             
+             e.dir = new direction(angle)
+         }
+         
+     }
+}
+
+
+function getRandomInteger( min, max ){
+    var difference = max - min;
+    
+    var number = parseInt(Math.round(Math.random() * difference + min));
+    return number;
+}
+
+function addEnemy() {
+    
+    
+    if(canAdd){
+    var newX = 0;
+    var newY = 0;
+    var counter = 0;
+    
+    var flag = false;
+    while(flag === false && counter < 1000){
+        newX = getRandomInteger(eSize, width - eSize);
+        newY = getRandomInteger(eSize, height - eSize);
+      
+        flag = true;
+        for(k = 0; k < enemies.length; k++){
+            var distance = Math.sqrt(Math.pow((newX - enemies[k].x),2) + Math.pow((newY - enemies[k].y),2));
+                if(distance < 2 * eSize){
+                    flag = false;
+                }  
+        }
+        var distance = Math.sqrt(Math.pow((newX - p.x),2) + Math.pow((newY - p.y),2));
+        if(distance < eSize + p.r){
+            flag = false;
+        }
+        counter = counter + 1;
+        
+        if(counter >= 1000) canAdd = false;
+        
+    }
+    if(counter < 1000){
+        
+      
+    var dir = (parseFloat(getRandomInteger(0,360)) / 360) * 2* Math.PI;
+    
+    enemies.push(new enemy(newX, newY, eSize, enemySpeed, new direction(dir),getRandomInteger(0,4),getRandomInteger(0,3),0));
+    
+    }
+}
+}
